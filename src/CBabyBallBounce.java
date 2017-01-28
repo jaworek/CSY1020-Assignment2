3296dc4dcad611f599cd17821450a5fe8a60c7cf
jaworek <jaworek3211@gmail.com>
2017-01-28 16:21:31 +0000

    Menubar and button icons

--- src/CBabyBallBounce.java
+++ src/CBabyBallBounce.java
@@ -2,7 +2,12 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class CBabyBallBounce extends JFrame implements ActionListener {
+public class CBabyBallBounce extends JFrame implements ActionListener
+{
+	// menu
+	private JMenuBar menuBar;
+	private JMenu scenarioMenu, editMenu, controlsMenu, helpMenu;
+	private JMenuItem exitItem, helpTopicItem, aboutItem;
 
 	// panels
 	private JPanel centerPnl, bottomPnl, rightPnl;
@@ -16,8 +21,8 @@ public class CBabyBallBounce extends JFrame implements ActionListener {
 	private JSlider speedSld;
 
 	// first panel
-	private JLabel timerLbl, colon1Lbl, colon2Lbl, scoreLbl, teamLbl;
-	private JTextField hours, minutes, seconds, scoreL, scoreR;
+	private JLabel timerLbl, colon1Lbl, colon2Lbl, TxtScoreLbl, teamLbl;
+	private JTextField TxtHours, TxtMinutes, TxtSeconds, TxtScoreL, TxtScoreR;
 
 	// second panel
 	private JLabel optionLbl, squareLbl, directionLbl;
@@ -31,130 +36,211 @@ public class CBabyBallBounce extends JFrame implements ActionListener {
 
 	// fifth panel
 	private JButton twoPlayerBtn, fourPlayerBtn, multiBtn, exitBtn;
+	
+	//images
+	private ImageIcon ballIcon, runIcon, actIcon, resetIcon;
 
-	public static void main(String[] args) {
+	private Container window;
+
+	public static void main(String[] args)
+	{
 		CBabyBallBounce frame = new CBabyBallBounce();
 		frame.setSize(825, 585);
 		frame.createGUI();
 		frame.setVisible(true);
 	}
 
-	private void createGUI() {
+	private void createGUI()
+	{
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		Container window = getContentPane();
-		window.setLayout(new FlowLayout());
+		window = getContentPane();
+		window.setLayout(new BorderLayout());
+		setTitle("CBabyBallBounce - Baby Ball Bounce Application");
+
+		// menu
+		menuBar();
 
 		// panel creation
+		centerPnl();
+		rightPnl();
+		bottomPnl();
+	}
+
+	private void menuBar()
+	{
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+
+		//scenario
+		scenarioMenu = new JMenu("Scenario");
+		menuBar.add(scenarioMenu);
+
+		exitItem = new JMenuItem("Exit");
+		scenarioMenu.add(exitItem);
+		
+		//edit
+		editMenu = new JMenu("Edit");
+		menuBar.add(editMenu);
+		
+		//controls
+		controlsMenu = new JMenu("Controls");
+		menuBar.add(controlsMenu);
+		
+		//help
+		helpMenu = new JMenu("Help");
+		menuBar.add(helpMenu);
+		
+		helpTopicItem = new JMenuItem("Help Topic");
+		helpMenu.add(helpTopicItem);
+		aboutItem = new JMenuItem("About");
+		helpMenu.add(aboutItem);
+	}
+
+	private void centerPnl()
+	{
 		centerPnl = new JPanel();
-		centerPnl.setPreferredSize(new Dimension(600, 485));
+		// centerPnl.setPreferredSize(new Dimension(600, 485));
 		centerPnl.setBackground(Color.white);
-		window.add(centerPnl);
+		window.add(centerPnl, BorderLayout.CENTER);
+	}
 
+	private void rightPnl()
+	{
 		rightPnl = new JPanel();
-		rightPnl.setPreferredSize(new Dimension(200, 485));
+		rightPnl.setLayout(new GridLayout(5, 1));
+		// rightPnl.setPreferredSize(new Dimension(200, 485));
 		rightPnl.setBackground(Color.red);
-		window.add(rightPnl);
+		window.add(rightPnl, BorderLayout.EAST);
 
+		firstPnl();
+		secondPnl();
+		thirdPnl();
+		fourthPnl();
+		fifthPnl();
+	}
+
+	private void bottomPnl()
+	{
 		bottomPnl = new JPanel();
-		bottomPnl.setPreferredSize(new Dimension(825, 100));
+		// bottomPnl.setPreferredSize(new Dimension(825, 50));
 		bottomPnl.setBackground(Color.gray);
-		window.add(bottomPnl);
+		window.add(bottomPnl, BorderLayout.SOUTH);
 
-		// right panel
+		actIcon = new ImageIcon("images/step.png");
+		actBtn = new JButton("Act", actIcon);
+		bottomPnl.add(actBtn);
+		actBtn.addActionListener(this);
+
+		runIcon = new ImageIcon("images/run.png");
+		runBtn = new JButton("Run", runIcon);
+		bottomPnl.add(runBtn);
+		runBtn.addActionListener(this);
+
+		resetIcon = new ImageIcon("images/reset.png");
+		resetBtn = new JButton("Reset", resetIcon);
+		bottomPnl.add(resetBtn);
+		resetBtn.addActionListener(this);
+
+		speedLbl = new JLabel("Speed: ");
+		bottomPnl.add(speedLbl);
+
+		speedSld = new JSlider();
+		bottomPnl.add(speedSld);
+	}
+
+	private void firstPnl()
+	{
 		firstPnl = new JPanel();
 		firstPnl.setPreferredSize(new Dimension(200, 90));
 		firstPnl.setBackground(Color.gray);
 		rightPnl.add(firstPnl);
 
-		secondPnl = new JPanel();
-		secondPnl.setPreferredSize(new Dimension(200, 90));
-		secondPnl.setBackground(Color.blue);
-		rightPnl.add(secondPnl);
-
-		thirdPnl = new JPanel();
-		thirdPnl.setPreferredSize(new Dimension(200, 90));
-		thirdPnl.setBackground(Color.yellow);
-		rightPnl.add(thirdPnl);
-
-		fourthPnl = new JPanel();
-		fourthPnl.setPreferredSize(new Dimension(200, 90));
-		fourthPnl.setBackground(Color.green);
-		rightPnl.add(fourthPnl);
-
-		fifthPnl = new JPanel();
-		fifthPnl.setPreferredSize(new Dimension(200, 90));
-		fifthPnl.setBackground(Color.orange);
-		rightPnl.add(fifthPnl);
-
-		// first panel
-		colon1Lbl = new JLabel(" : ");
-		colon2Lbl = new JLabel(" : ");
-
 		timerLbl = new JLabel("DIGITAL TIMER");
 		firstPnl.add(timerLbl);
 
-		hours = new JTextField("hh");
-		firstPnl.add(hours);
+		TxtHours = new JTextField("hh");
+		firstPnl.add(TxtHours);
 
+		colon1Lbl = new JLabel(" : ");
 		firstPnl.add(colon1Lbl);
 
-		minutes = new JTextField("mm");
-		firstPnl.add(minutes);
+		TxtMinutes = new JTextField("mm");
+		firstPnl.add(TxtMinutes);
 
+		colon2Lbl = new JLabel(" : ");
 		firstPnl.add(colon2Lbl);
 
-		seconds = new JTextField("ss");
-		firstPnl.add(seconds);
+		TxtSeconds = new JTextField("ss");
+		firstPnl.add(TxtSeconds);
 
-		scoreLbl = new JLabel("SCORE");
-		firstPnl.add(scoreLbl);
+		TxtScoreLbl = new JLabel("SCORE");
+		firstPnl.add(TxtScoreLbl);
 
-		scoreL = new JTextField();
-		firstPnl.add(scoreL);
+		TxtScoreL = new JTextField();
+		firstPnl.add(TxtScoreL);
 
 		teamLbl = new JLabel("<L:R>");
 		firstPnl.add(teamLbl);
 
-		scoreR = new JTextField();
-		firstPnl.add(scoreR);
+		TxtScoreR = new JTextField();
+		firstPnl.add(TxtScoreR);
+	}
 
-		// second panel
-		optionLbl = new JLabel("Option:");
+	private void secondPnl()
+	{
+		secondPnl = new JPanel();
+		secondPnl.setLayout(new GridLayout(3, 2));
+		secondPnl.setPreferredSize(new Dimension(200, 90));
+		secondPnl.setBackground(Color.blue);
+		rightPnl.add(secondPnl);
+
+		optionLbl = new JLabel(" Option:");
 		secondPnl.add(optionLbl);
 
 		option = new JTextField();
 		secondPnl.add(option);
 
-		squareLbl = new JLabel("Square:");
+		squareLbl = new JLabel(" Square:");
 		secondPnl.add(squareLbl);
 
 		square = new JTextField();
 		secondPnl.add(square);
 
-		directionLbl = new JLabel("Direction:");
+		directionLbl = new JLabel(" Direction:");
 		secondPnl.add(directionLbl);
 
 		direction = new JTextField();
 		secondPnl.add(direction);
+	}
 
-		// third panel
-		upBtn = new JButton(" ");
-		thirdPnl.add(upBtn);
-		upBtn.addActionListener(this);
+	private void thirdPnl()
+	{
+		thirdPnl = new JPanel();
+		thirdPnl.setLayout(new GridLayout(3, 3));
+		thirdPnl.setPreferredSize(new Dimension(200, 90));
+		thirdPnl.setBackground(Color.yellow);
+		rightPnl.add(thirdPnl);
 
-		blank1Btn = new JButton("^");
+		blank1Btn = new JButton(" ");
 		thirdPnl.add(blank1Btn);
 		blank1Btn.addActionListener(this);
+		blank1Btn.setEnabled(false);
+
+		upBtn = new JButton("^");
+		thirdPnl.add(upBtn);
+		upBtn.addActionListener(this);
 
 		blank2Btn = new JButton(" ");
 		thirdPnl.add(blank2Btn);
 		blank2Btn.addActionListener(this);
+		blank2Btn.setEnabled(false);
 
 		leftBtn = new JButton("<");
 		thirdPnl.add(leftBtn);
 		leftBtn.addActionListener(this);
 
-		middleBtn = new JButton("Middle");
+		ballIcon = new ImageIcon("images/ball.png");
+		middleBtn = new JButton(ballIcon);
 		thirdPnl.add(middleBtn);
 		middleBtn.addActionListener(this);
 
@@ -165,6 +251,7 @@ public class CBabyBallBounce extends JFrame implements ActionListener {
 		blank3Btn = new JButton(" ");
 		thirdPnl.add(blank3Btn);
 		blank3Btn.addActionListener(this);
+		blank3Btn.setEnabled(false);
 
 		downBtn = new JButton("v");
 		thirdPnl.add(downBtn);
@@ -173,13 +260,30 @@ public class CBabyBallBounce extends JFrame implements ActionListener {
 		blank4Btn = new JButton(" ");
 		thirdPnl.add(blank4Btn);
 		blank4Btn.addActionListener(this);
+		blank4Btn.setEnabled(false);
+	}
 
-		// fourth panel
+	private void fourthPnl()
+	{
+		fourthPnl = new JPanel();
+		fourthPnl.setLayout(new GridLayout(1, 1));
+		fourthPnl.setPreferredSize(new Dimension(200, 90));
+		fourthPnl.setBackground(Color.green);
+		rightPnl.add(fourthPnl);
+
 		compassBtn = new JButton("Compass");
 		fourthPnl.add(compassBtn);
 		compassBtn.addActionListener(this);
+	}
 
-		// fifth panel
+	private void fifthPnl()
+	{
+		fifthPnl = new JPanel();
+		fifthPnl.setLayout(new GridLayout(2, 2));
+		fifthPnl.setPreferredSize(new Dimension(200, 90));
+		fifthPnl.setBackground(Color.orange);
+		rightPnl.add(fifthPnl);
+
 		twoPlayerBtn = new JButton("2 Player");
 		fifthPnl.add(twoPlayerBtn);
 		twoPlayerBtn.addActionListener(this);
@@ -195,27 +299,10 @@ public class CBabyBallBounce extends JFrame implements ActionListener {
 		exitBtn = new JButton("Exit");
 		fifthPnl.add(exitBtn);
 		exitBtn.addActionListener(this);
-
-		// bottom panel
-		actBtn = new JButton("Act");
-		bottomPnl.add(actBtn);
-		actBtn.addActionListener(this);
-
-		runBtn = new JButton("Run");
-		bottomPnl.add(runBtn);
-		runBtn.addActionListener(this);
-
-		resetBtn = new JButton("Reset");
-		bottomPnl.add(resetBtn);
-		resetBtn.addActionListener(this);
-
-		speedLbl = new JLabel("Speed: ");
-		bottomPnl.add(speedLbl);
-
-		speedSld = new JSlider();
-		bottomPnl.add(speedSld);
 	}
+	
 
-	public void actionPerformed(ActionEvent event) {
+	public void actionPerformed(ActionEvent event)
+	{
 	}
 }
